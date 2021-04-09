cc20e76ce30e190136d9c4d5b3ce1143f3cec87e
SignorinoY <meetziyang@gmail.com>
2021-04-10 02:39:40 +0800

    :recycle: refactor logistic regression of MATLAB

--- logistic_regression/logisticRegression.m
+++ logistic_regression/logisticRegression.m
@@ -1,19 +1,19 @@
-function [beta] = logisticRegression(Y,X,initBeta,maxIter,epsilon)
-%logisticRegression 此处显示有关此函数的摘要
-%   此处显示详细说明
+function beta = logisticRegression(X, y, beta0, MaxIter, epsilon)
+if nargin < 5, epsilon = 1e-8; end
+if nargin < 4, MaxIter = 50; end
 [n, ~] = size(X);
 X = [ones(n,1), X];
-beta = initBeta;
+beta = beta0;
 m = 1;
-for i=1:maxIter
-    betaOld = beta;
-    eta = X * betaOld;
+for i=1:MaxIter
+    beta0 = beta;
+    eta = X * beta0;
     pi = exp(eta) ./ (1. + exp(eta));
     mu = m * pi;
     W = diag(m * pi .* (1 - pi));
-    z = eta + W ^ (-1) * (Y - mu);
+    z = eta + W ^ (-1) * (y - mu);
     beta = (X' * W * X + 1e-8) ^ (-1) * X' * W * z;
-    if(norm(beta - betaOld) < epsilon)
+    if(norm(beta - beta0) < epsilon)
         break
     end
 end
--- logistic_regression/test.m
+++ logistic_regression/test.m
@@ -3,9 +3,9 @@ clc, clear
 n = 2000;
 beta = [0, 1, 1.5]';
 
-[Y, X] = faker(n, beta, 1);
+[X, y] = faker(n, beta, 1);
 
-initBeta = [0; 0; 0];
-maxIter = 50;
+beta0 = [0; 0; 0];
+MaxIter = 50;
 epsilon = 1e-8;
-betaHat = logisticRegression(Y,X,initBeta,maxIter,epsilon)
+betaHat = logisticRegression(X,y,beta0,MaxIter,epsilon)
