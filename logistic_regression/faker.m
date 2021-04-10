feb8bf982699d2148d950090a6a8efb60a3dd0ef
SignorinoY <meetziyang@gmail.com>
2021-04-10 13:11:33 +0800

    :recycle: refactor code with MBeaurify

--- logistic_regression/faker.m
+++ logistic_regression/faker.m
@@ -1,10 +1,7 @@
-function [X,y] = faker(n, beta, seed)
-%FAKER 此处显示有关此函数的摘要
-%   此处显示详细说明
+function [X, y] = faker(n, beta, seed)
 rng(seed);
-X = [randn(n,1), rand(n,1) < 0.5];
-eta = [ones(n,1), X] * beta;
+X = [randn(n, 1), rand(n, 1) < 0.5];
+eta = [ones(n, 1), X] * beta;
 pi = exp(eta) ./ (1. + exp(eta));
-y = rand(n,1) < pi;
+y = rand(n, 1) < pi;
 end
-
--- logistic_regression/logisticRegression.m
+++ logistic_regression/logisticRegression.m
@@ -1,27 +1,20 @@
 function beta = logisticRegression(X, y, beta0, MaxIter, epsilon)
-%logisticRegression This MATLAB function fits the logistic regression model with (X, y), and returns the estimated parameters.
-%    X n * p dim matrix of covariates, do not contain a column of 1s as an intercept
-%    y n * 1 dim matrix of binary reponse
-%    beta0 (p + 1) dim vector of initial value for beta
-%    MaxIter maximum number of iterations, the default is 50
-%    epsilon maximum termination tolerance error, the default is 1e-8
-if nargin < 5, epsilon = 1e-8; end
-if nargin < 4, MaxIter = 50; end
+if nargin < 5, epsilon = 1e-8;end
+if nargin < 4, MaxIter = 50;end
 [n, ~] = size(X);
-X = [ones(n,1), X];
+X = [ones(n, 1), X];
 beta = beta0;
 m = 1;
-for i=1:MaxIter
+for iter = 1:MaxIter
     beta0 = beta;
     eta = X * beta0;
     pi = exp(eta) ./ (1. + exp(eta));
     mu = m * pi;
     W = diag(m * pi .* (1 - pi));
-    z = eta + W ^ (-1) * (y - mu);
-    beta = (X' * W * X + 1e-8) ^ (-1) * X' * W * z;
-    if(norm(beta - beta0) < epsilon)
+    z = eta + W^(-1) * (y - mu);
+    beta = (X' * W * X)^(-1) * X' * W * z;
+    if (norm(beta - beta0) < epsilon)
         break
     end
 end
 end
-
