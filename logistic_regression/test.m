eb03aca95c9593c9e55b5c74c7d7b0b95a03302e
SignorinoY <meetziyang@gmail.com>
2021-04-10 19:44:40 +0800

    :white_check_mark: add test for logistic regression with/without intercept

--- logistic_regression/test.m
+++ logistic_regression/test.m
@@ -2,16 +2,36 @@ clc, clear
 
 % Parameters
 n = 200;
+repeat = 100;
+
+%% With Intercept
+% Parameters
 beta = [0, 1, 1.5]';
 beta0 = [0; 0; 0];
-repeat = 100;
 
 % Simulation
 bias = zeros(repeat, 3);
 for seed = 1:repeat
     [X, y] = faker(n, beta, seed);
-    betaHat = logisticRegression(X, y, beta0);
+    betaHat = logisticRegression(X, y, beta0, true);
     bias(seed, :) = betaHat - beta;
+end
+
+% Estimated Result
+mean(bias)
+std(bias)
+
+%% Without Intercept
+% Parameters
+beta = [0, 1, 1.5]';
+beta0 = [0; 0];
+
+% Simulation
+bias = zeros(repeat, 2);
+for seed = 1:repeat
+    [X, y] = faker(n, beta, seed);
+    betaHat = logisticRegression(X, y, beta0, false);
+    bias(seed, :) = betaHat - beta(2:3);
 end
 
 % Estimated Result
